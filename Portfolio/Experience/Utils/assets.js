459bdde2f18c33b08ca25f71a6fb6cadd2904237
Daniel <daniel@danielwjyoung.com>
2022-12-20 10:03:23 -0600

    first commit

---
+++ Portfolio/Experience/Utils/assets.js
@@ -0,0 +1,12 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Final-bedroom.glb",
+    },
+    {
+        name: "screenOne",
+        type: "videoTexture",
+        path: "/textures/viperAceClutch.mp4",
+    }
+]
---
+++ Portfolio/Experience/World/Room.js
@@ -0,0 +1,45 @@
+import * as THREE from "three";
+
+import Experience from "../Experience";
+
+export default class Room {
+    constructor(){
+        this.experience = new Experience();
+        this.scene = this.experience.scene;
+        this.resources = this.experience.resources;
+        this.room = this.resources.items.room;
+        this.actualRoom = this.room.scene;
+
+
+        this.setModel();
+    }
+
+    setModel(){
+        this.actualRoom.children.forEach((child) =>{
+            child.castShadow = true;
+            child.receiveShadow = true;
+            if(child instanceof THREE.Group){
+                child.children.forEach((groupChild) => {
+                    groupChild.castShadow = true;
+                    groupChild.receiveShadow = true;
+                })
+
+            }
+
+            if(child.name === "Screen1"){
+                console.log(child);
+                child.material = new THREE.MeshBasicMaterial({
+                    map: this.resources.items.screenOne,
+                });
+            }
+
+        });
+
+        this.scene.add(this.actualRoom);
+    }
+
+    resize(){} 
+    
+
+    update(){}
+}
